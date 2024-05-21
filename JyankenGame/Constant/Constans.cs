3c148dcf62ef995cf8181e9c255fc710b23fdebc
re-hiramatsu <re-hiramatsu>
2024-05-21 14:03:22 +0900

    コンスタンス修正

--- JyankenGame/Constant/Constans.cs
+++ JyankenGame/Constant/Constans.cs
@@ -9,7 +9,7 @@ namespace JyankenGame.Constant
     static class Constans
     {
         public static readonly string SELECTION_MESSAGE = "じゃんけんをする：１　通算成績を見る：２　終了する：９";
-        public static readonly string HANDSIGNAL_MESSAGE = "じゃんけんの手札を入力してください(グー：0　チョキ：1　パー：2)";
+        public static readonly string HANDSIGNAL_MESSAGE = "じゃんけんの手札を入力してください(グー：０　チョキ：１　パー：２)";
         public static readonly string HANDSIGNAL_ERROR_MESSAGE = "手の形が入力されていません";
         public static readonly string PLAYER_WIN_MESSAGE = "あなた：{0}、BOT：{1} であなたの勝ち！";
         public static readonly string PLAYER_LOSE_MESSAGE = "あなた：{0}、BOT：{1} であなたの負け！";
